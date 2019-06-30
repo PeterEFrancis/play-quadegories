67de3c7ef0d4984715cc693a87b9032af86990d0
francisp336 <40155448+francisp336@users.noreply.github.com>
2019-06-30 00:39:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return 'Hello World!'
+  return render_template('index.html', generated = 'testing,testing, 123'
   
