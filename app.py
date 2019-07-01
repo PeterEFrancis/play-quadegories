16e788b4b82a9940563d7532789e56a379427451
Peter Francis <francisp336@gmail.com>
2019-07-01 02:07:22 -0400

    edits

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def main():
 def game():
     quad = 'quad'
     fact = 'fact'
-    clue4 = 'clue4'
-    clue3 = 'clue3'
-    clue2 = 'clue2'
-    clue1 = 'clue1'
+    clue4 = 'clue #4'
+    clue3 = 'clue #3'
+    clue2 = 'clue #2'
+    clue1 = 'clue #1'
     return render_template('game.html', quad = quad, fact = fact, clue4 = clue4,
     clue3 = clue3, clue2 = clue2, clue1 = clue1)
 
