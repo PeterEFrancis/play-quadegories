d67e949ba203e32b7b0219f855782874bb490b13
Peter Francis <francisp336@gmail.com>
2019-07-01 04:07:27 -0400

    edit app.py

--- app.py
+++ app.py
@@ -21,6 +21,19 @@ for i in range(len(quadegories)):
     quadegories[i] = re.split('\\n\\\\ansP', quadegories[i])
     # remove braces
     quadegories[i] = [re.split('}{', quadegories[i][0][1:-1]), re.split('}{', quadegories[i][1][1:-2])]
+    # replace `` and reformat to single lines
+    quadegories[i] = [re.sub('``', '&#8220', string) for string in quadegories[i][0] + quadegories[i][1]]
+    # replace '
+    quadegories[i] = [re.sub('`', '&#39', string) for string in quadegories[i]]
+    # replace bold statements    \\textbf{string} --> <strong>string</strong>
+
+    # replace italics statements    \\textit{string} --> <i>string</i>
+
+    # replace sc statements       \\textsc{string} --> <span style="font-variant:small-caps;">string</span>
+
+    # delete extra newlines \\
+    quadegories[i] = [re.sub('\\', '', string) for string in quadegories[i]]
+
 text_file.close()
 
 
@@ -31,7 +44,7 @@ def game():
     clue3 = clue3, clue2 = clue2, clue1 = clue1)
 
 
-    
+
 
 if __name__ == "__main__":
     app.run(port=5002)
