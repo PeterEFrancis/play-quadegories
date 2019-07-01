f242c8021865097e71f94b02d2b951d72d5b4ddb
Peter Francis <francisp336@gmail.com>
2019-07-01 03:36:32 -0400

    massive edits

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, url_for
+import re
+import random
+
 
 app = Flask(__name__)
 
@@ -6,14 +9,24 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
+# load in quadegoties.txt
+text_file = open("quadegories.txt", "r")
+lines = text_file.readlines()
+
+text = ''.join(line if line[0]=='\\' else '' for line in open("quadegories.txt", "r").readlines())
+quadegories = re.split('\\\\listP', text)[1:]
+for i in range(len(quadegories)):
+    # Split into quads and clues
+    quadegories[i] = re.split('\\n\\\\ansP', quadegories[i])
+    # remove braces
+    quadegories[i] = [re.split('}{', quadegories[i][0][1:-1]), re.split('}{', quadegories[i][1][1:-2])]
+text_file.close()
+
+
 @app.route('/game')
 def game():
-    quad = 'quad'
-    fact = 'fact'
-    clue4 = 'clue #4'
-    clue3 = 'clue #3'
-    clue2 = 'clue #2'
-    clue1 = 'clue #1'
+    [[clue4, clue3, clue2, clue1], [quad, fact]] = quadegories[random.randint(0,len(quadegories))]
     return render_template('game.html', quad = quad, fact = fact, clue4 = clue4,
     clue3 = clue3, clue2 = clue2, clue1 = clue1)
 
