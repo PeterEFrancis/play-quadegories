93604190e57570752939a30c348f124cad077a48
Peter Francis <francisp336@gmail.com>
2019-07-03 19:19:57 -0400

    fix css link

--- app.py
+++ app.py
@@ -26,34 +26,40 @@ def format(string):
     return string
 
 
-# read in quadegories.txt
-text = ''.join(line if line[0]=='\\' else '' for line in open("quadegories/general.txt", "r").readlines())
-quadegories = re.split('\\\\listP', text)[1:]
-# format text
-for i in range(len(quadegories)):
-    # Split into quads and clues
-    quadegories[i] = re.split('\\n\\\\ansP', quadegories[i])
-    # remove braces
-    quadegories[i] = [re.split('}{', quadegories[i][0][1:-1]), re.split('}{', quadegories[i][1][1:-2])]
-    # replace `` and reformat to single lines
-    quadegories[i] = [re.sub('``', '\"', string) for string in quadegories[i][0] + quadegories[i][1]]
-    # replace ''
-    # replace '
-    quadegories[i] = [re.sub('`', '\'', string) for string in quadegories[i]]
-    # delete \\
-    quadegories[i] = [re.sub('\\\\', '', string) for string in quadegories[i]]
-    # replace bold statements    textbf{string} --> <strong>string</strong>
-    # replace italics statements    textit{string} --> <em>string</em>
-    # replace sc statements       textsc{string} --> <span style="font-variant:small-caps;">string</span>
-    quadegories[i] = [format(string) for string in quadegories[i]]
 
 
 @app.route('/play')
 def play():
     return redirect('/game/' + str(random.randint(0,len(quadegories)-1)))
 
-@app.route('/game/<int:choice>')
-def game(choice):
+
+@app.route('/game')
+def auto():
+    return redirect('/game/general/1')
+
+
+@app.route('/game/<string:catagory>/<int:choice>')
+def game(catagory, choice):
+    # read in quadegories.txt
+    text = ''.join(line if line[0]=='\\' else '' for line in open(f"quadegories/{catagory}.txt", "r").readlines())
+    quadegories = re.split('\\\\listP', text)[1:]
+    # format text
+    for i in range(len(quadegories)):
+        # Split into quads and clues
+        quadegories[i] = re.split('\\n\\\\ansP', quadegories[i])
+        # remove braces
+        quadegories[i] = [re.split('}{', quadegories[i][0][1:-1]), re.split('}{', quadegories[i][1][1:-2])]
+        # replace `` and reformat to single lines
+        quadegories[i] = [re.sub('``', '\"', string) for string in quadegories[i][0] + quadegories[i][1]]
+        # replace ''
+        # replace '
+        quadegories[i] = [re.sub('`', '\'', string) for string in quadegories[i]]
+        # delete \\
+        quadegories[i] = [re.sub('\\\\', '', string) for string in quadegories[i]]
+        # replace bold statements    textbf{string} --> <strong>string</strong>
+        # replace italics statements    textit{string} --> <em>string</em>
+        # replace sc statements       textsc{string} --> <span style="font-variant:small-caps;">string</span>
+        quadegories[i] = [format(string) for string in quadegories[i]]
     [clue4, clue3, clue2, clue1, quad, fact] = quadegories[choice]
     return render_template('game.html', quad = quad, fact = fact, clue4 = clue4,
                                         clue3 = clue3, clue2 = clue2, clue1 = clue1)
