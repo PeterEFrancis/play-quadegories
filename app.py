6b229d9fffe46a9f71fa084bff4056b6ddc8b3f2
Peter Francis <francisp336@gmail.com>
2019-12-21 22:25:27 -0500

    updates

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import html
 import os
 
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,65 +14,17 @@ def index():
     return render_template('index.html')
 
 
-def format(string):
-    while re.search("{", string) is not None:
-        # find the first { and set the start of the string as the character index after
-        start = re.search("{", string).start() + 1
-        # find the next } and set the end of the string as that characte index
-        end = re.search("}", string).start() - 1
-        # while there is a { in between start and end, reset start
-        while re.search("{", string[start:end+1]) is not None:
-            start += re.search("{", string[start:end+1]).start() + 1
-        # replace the 6 characters before start with html and replace } with the closing html
-        front = {
-            'bf':'<strong style=\"font-size:inherit\">',
-            'it':'<em style=\"font-size:inherit\">',
-            'sc':'<span style=\"font-variant:small-caps; font-size:inherit;\">'
-        }
-        back = {
-            'bf':'</strong>',
-            'it':'</em>',
-            'sc':'</span>'
-        }
-        string = string[:start-7] + front[string[start-3:start-1]] + string[start:end+1]+ back[string[start-3:start-1]]+ string[end+2:]
-    return string
 
-def loadIn(collection):
-    text = ''.join(line  if (line[0]=='\\' or line[0:2] == '%!') else '' for line in open(f'quadegories/{collection}', 'r').readlines())
-    # split at the end of description
-    parts = re.split('end{Description}', text)
-    # recover description
-    description = parts[0][20:-2]
-    # sort quadegories
-    quadegories = re.split('\\\\listP', parts[1])[1:]
-    # format
-    for i in range(len(quadegories)):
-        # Split into quads and clues
-        quadegories[i] = re.split('\\n\\\\ansP', quadegories[i])
-        # remove braces
-        quadegories[i] = [re.split('}{', quadegories[i][0][1:-1]), re.split('}{', quadegories[i][1][1:-2])]
-        # replace `` and reformat to single lines
-        quadegories[i] = [re.sub('``', '\\\\"', string) for string in quadegories[i][0] + quadegories[i][1]]
-        # replace ''
-            # ?
-        # replace '
-        quadegories[i] = [re.sub('`', "\\'", string) for string in quadegories[i]]
-        # delete \\
-        quadegories[i] = [re.sub('\\\\', '', string) for string in quadegories[i]]
-        # replace bold statements       textbf{string} --> <strong>string</strong>
-        # replace italics statements    textit{string} --> <em>string</em>
-        # replace sc statements         textsc{string} --> <span style="font-variant:small-caps;">string</span>
-        quadegories[i] = [format(string) for string in quadegories[i]]
-    return description, quadegories
-
-
-
-# load in all collection files
-collectionDict = {}
 descriptionDict = {}
-for file in os.listdir('quadegories'):
-    if os.fsdecode(file).endswith('.txt'):
-        descriptionDict[os.fsdecode(file)[:-4]], collectionDict[os.fsdecode(file)[:-4]] = loadIn(file)
+collectionDict = {}
+# load in all collection files
+
+from quadegories.beginner import *
+descriptionDict['Beginner'], collectionDict['Beginner'] = beginnerTup
+from quadegories.expert import *
+descriptionDict['Expert'], collectionDict['Expert'] = expertTup
+
+
 
 
 @app.route('/choose-collection')
@@ -121,4 +74,4 @@ def game(collection, choice):
 
 
 if __name__ == "__main__":
-    app.run(port=5002)
+    app.run(port=1969)
